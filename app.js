cb9ff8972a0428f034735c2c0cfaaad5682b235b
Dana-lil <dana.alajbur@gmail.com>
2025-05-05 17:06:04 +0300

    done

--- app.js
+++ app.js
@@ -1,11 +1,78 @@
-/*-------------------------------- Constants --------------------------------*/
-const Titlecalc =document.querySelector('#calculator');
-console.dir(Titlecalc);
+let firstNum=null;
+let seconedNum=null;
+let operation =null;
+let total;
 
-/*-------------------------------- Variables --------------------------------*/
 
-/*------------------------ Cached Element References ------------------------*/
+// catched ellemnets 
+const buttons =document.querySelectorAll('.button'); // because its not an id 
+const calculator = document.querySelector('#calculator');
+const show = document.querySelector('.display');
+show.innerText=0;
 
-/*----------------------------- Event Listeners -----------------------------*/
 
-/*-------------------------------- Functions --------------------------------*/
+
+//
+
+calculator.addEventListener('click',(event)=>{
+    console.log(event.target.innerText);
+
+    if(event.target.classList.contains('number')){
+
+    if (firstNum === null && operation === null && seconedNum === null){
+        firstNum =event.target.innerText;
+        show.innerText=firstNum;
+    }
+    else if( firstNum !== null && operation === null && seconedNum ===null){
+        if (firstNum===total)firstNum =event.target.innerText;
+        show.innerText =seconedNum
+    }
+    else if (firstNum!== null && operation!== null && seconedNum === null){
+        seconedNum=event.target.innerText;
+        show.innerText = seconedNum
+    }
+    else if ( firstNum !== null && operation !== null && seconedNum !== null){
+        seconedNum+=event.target.innerText;
+        show.innerText =seconedNum
+    }
+     console.log("firstNumber =", firstNum, "seconedNumber =" , seconedNum);
+    }
+
+ if (event.target.classList.contains('operator')){
+if(event.target.innerText==='C'){
+    show.innerText =0;
+    firstNum = null;
+    operation = null ;
+    console.log("clear");
+}
+else {
+    operation=event.target.innerText;
+    console.log('selected operation'+ operation); 
+}
+}
+if ( event.target.innerText ==='='){
+     
+
+    firstNum =firstNum*1
+    seconedNum =seconedNum *1
+     
+    if (operation === '+') {
+        total =firstNum +seconedNum;
+    }
+    else if (operation === '-'){
+        total = firstNum =seconedNum;
+    }
+    else if (operation === '*'){
+        total = firstNum*seconedNum;
+    }
+    else if ( operation === '/'){
+        total = firstNum / seconedNum;
+    }
+
+
+    show.innerText=total;
+    firstNum=total;
+    operation = null;
+    seconedNum= null;
+}
+});
